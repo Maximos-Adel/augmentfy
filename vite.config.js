641bca05518cedbfa4a89a7ae1e24d75c3615350
Maximos-Adel <maximosadel@gmail.com>
2025-01-15 00:22:31 +0200

    vercel

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,25 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
-  server: {
-    proxy: {
-      // Example: Proxy requests to "/model" to the external server
-      '/model': {
-        target: 'https://assets.meshy.ai', // Target the URL hosting the models
-        changeOrigin: true, // Change the origin of the request to the target URL
-        secure: false, // Set to false if the target server uses self-signed certificates
-        rewrite: (path) => path.replace(/^\/model/, ''), // Strip the "/model" prefix
-      },
-      '/storage': {
-        target: 'https://pmpgkanoddidpuwqspzi.supabase.co',
-        changeOrigin: true,
-      },
-    },
-  },
-  test: /\.(glb|gltf)$/,
-  use: [
-    {
-      loader: 'file-loader',
-    },
-  ],
+  // server: {
+  //   proxy: {
+  //     // Example: Proxy requests to "/model" to the external server
+  //     '/model': {
+  //       target: 'https://assets.meshy.ai', // Target the URL hosting the models
+  //       changeOrigin: true, // Change the origin of the request to the target URL
+  //       secure: false, // Set to false if the target server uses self-signed certificates
+  //       rewrite: (path) => path.replace(/^\/model/, ''), // Strip the "/model" prefix
+  //     },
+  //     '/storage': {
+  //       target: 'https://pmpgkanoddidpuwqspzi.supabase.co',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
+  // test: /\.(glb|gltf)$/,
+  // use: [
+  //   {
+  //     loader: 'file-loader',
+  //   },
+  // ],
 });
