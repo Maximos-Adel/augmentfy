5a4ce75f76e46c988282e645426fd6d6fc47887e
Maximos-Adel <maximosadel@gmail.com>
2025-01-19 13:24:41 +0200

    test

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -2,9 +2,18 @@ const ModelViewer = ({ glbUrl }) => {
   return (
     <div>
       {/* 3D Model Viewer */}
-      <model-viewer
+      {/* <model-viewer
         alt="3D Model"
         src={`/mesh${glbUrl}`}
+        ar
+        auto-rotate
+        crossorigin="anonymous"
+        camera-controls
+        style={{ width: '100%', height: '500px' }}
+      ></model-viewer> */}
+      <model-viewer
+        alt="3D Model"
+        src={glbUrl} // Ensure glbUrl contains the full absolute URL
         ar
         auto-rotate
         crossorigin="anonymous"
